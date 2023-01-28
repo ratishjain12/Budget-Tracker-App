58ddc692c9f2c62310d3819cd66db2c4866d35d0
Kaushal Padaliya <kaushalpadaliya@Kaushals-Air.Optilink>
2023-01-28 19:07:28 +0530

    Google sign in  complete

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,8 +1,10 @@
 import 'package:budget_tracker/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   FirebaseAuth auth = FirebaseAuth.instance;
+  final googleSignIn = GoogleSignIn();
 
   Future emailPasswordSignUp(
       String email, String password, String username) async {
@@ -30,5 +32,31 @@ class AuthService {
     } on FirebaseAuthException catch (e) {
       return e.message;
     }
+  }
+
+  Future<bool> googleSignin() async {
+    GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+    if (googleSignInAccount != null) {
+      GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      try {
+        UserCredential result = await auth.signInWithCredential(credential);
+      } catch (e) {
+        print(e);
+      }
+      return Future.value(true);
+    }
+    return Future.value(false);
+  }
+
+  Future signOut() async {
+    User? user = await auth.currentUser;
+    await googleSignIn.signOut();
+    await auth.signOut();
   }
 }
