3100724db9db1771966ee9aabc10d8a1b54269e3
ratish jain <ratishjain@ratishs-MacBook-Air.local>
2023-02-01 12:20:58 +0530

    homepage design

---
+++ lib/widgets/expense_tile.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class ExpenseTile extends StatefulWidget {
+  final String title;
+  final int money;
+  final String date;
+
+  const ExpenseTile({
+    Key? key,
+    required this.title,
+    required this.money,
+    required this.date,
+  }) : super(key: key);
+
+  @override
+  State<ExpenseTile> createState() => _ExpenseTileState();
+}
+
+class _ExpenseTileState extends State<ExpenseTile> {
+  @override
+  Widget build(BuildContext context) {
+    print(widget.date);
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(8),
+      width: double.infinity,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 6.0,
+            spreadRadius: 1,
+            color: Colors.grey,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 7.0),
+                child: Text(
+                  "-" + widget.money.toString() + " " + "\u{20B9}",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.date,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Colors.grey[700],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+// // Text(
+//                     widget.date,
+//                     style: TextStyle(fontSize: 16, color: Colors.black),
+//                   ),
