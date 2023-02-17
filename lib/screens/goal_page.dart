cd869d7900414ab8e410d3e7203426223adc7464
kaushal1717 <kpadaliya1717@gmail.com>
2023-02-17 11:37:05 +0530

    commit

--- lib/screens/goal_page.dart
+++ lib/screens/goal_page.dart
@@ -1,8 +1,6 @@
-import 'dart:ffi';
-
 import 'package:budget_tracker/widgets/colors.dart';
+import 'package:budget_tracker/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class GoalPage extends StatefulWidget {
@@ -12,7 +10,22 @@ class GoalPage extends StatefulWidget {
   State<GoalPage> createState() => _GoalPageState();
 }
 
-class _GoalPageState extends State<GoalPage> {
+class _GoalPageState extends State<GoalPage>
+    with AutomaticKeepAliveClientMixin {
+  final _formKey = GlobalKey<FormState>();
+  final _goalnameController = TextEditingController();
+  final _goalController = TextEditingController();
+
+  @override
+  bool get wantKeepAlive => true;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _goalnameController.dispose();
+    _goalController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
@@ -21,6 +34,15 @@ class _GoalPageState extends State<GoalPage> {
       backgroundColor: AppColors.primaryColor,
       appBar: AppBar(
         backgroundColor: AppColors.secondaryColor,
+        title: Text("Budget Tracker"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.secondaryColor,
+        child: Icon(Icons.add),
+        onPressed: () {
+          popupDialog(context);
+        },
       ),
       body: SafeArea(
         child: Column(
@@ -83,5 +105,89 @@ class _GoalPageState extends State<GoalPage> {
         ),
       ),
     );
+  }
+
+  popupDialog(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: ((context, setState) {
+            return Center(
+              child: AlertDialog(
+                title: Text(
+                  "Add goal",
+                  textAlign: TextAlign.center,
+                ),
+                content: Container(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          controller: _goalnameController,
+                          keyboardType: TextInputType.text,
+                          validator: (val) {
+                            if (val == null || val.isEmpty) {
+                              return "Please enter value";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            isDense: true,
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: AppColors.secondaryColor),
+                            ),
+                            border: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: AppColors.secondaryColor),
+                            ),
+                            hintText: 'Goal name',
+                          ),
+                        ),
+                        SizedBox(
+                          height: screenWidth * 0.02,
+                        ),
+                        TextFormField(
+                          controller: _goalController,
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Please Enter Value";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            isDense: true,
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: AppColors.secondaryColor),
+                            ),
+                            border: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: AppColors.secondaryColor),
+                            ),
+                            hintText: 'Amount',
+                          ),
+                        ),
+                        SizedBox(
+                          height: screenHeight * 0.02,
+                        ),
+                        CustomButton(
+                            child: Text("Submit"), onPressed: () async {
+                              
+                            })
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }));
+        });
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Database {
   final String? uid;
   Database({this.uid});
-
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection("users");
 
